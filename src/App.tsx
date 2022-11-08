575432cd6e7d9ae6883970a5e6128eb6c1ead490
王超 <mikewang199333@163.com>
2022-11-08 20:10:01 +0800

    ggg

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: wangchao
+ * @Date: 2022-11-08 19:50:18
+ */
+import { Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+function App() {
+  return <Container>Hi</Container>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Description:
+ * @Author: wangchao
+ * @Date: 2022-11-08 19:50:18
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
